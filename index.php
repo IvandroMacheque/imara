4914b264eb8c2549c071d563583a4c808e48b899
Victor Langa <machequeivandro444@gmail.com>
2026-04-22 15:16:43 +0200

    Fix: Site limpo e corrigido sem virus

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// Script corrigido - Removida injeção de código remoto
+
+$index_home = __DIR__ . '/index.html';
+
+if (is_file($index_home)) {
+    include $index_home;
+} else {
+    http_response_code(404);
+    echo 'Erro crítico: Arquivo principal do site não encontrado.';
+}
+?>
